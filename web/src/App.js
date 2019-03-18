7b8ee0d7e44e986a0293a0c2b49395f4e2d8e50c
Sergio Miranda <sjamr10@gmail.com>
2019-03-18 12:47:15 -0300

    Add redux stuff

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from './actions/simpleAction';
 import './App.css';
 
 class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+   }
+
   render() {
     return (
       <div className="App">
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
         <header className="App-header">
+          <button onClick={this.simpleAction}>Test redux action</button>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    ...state
+  }), 
+  dispatch => ({
+    simpleAction: () => dispatch(simpleAction())
+ }))(App);
