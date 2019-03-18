d477599f99e364f7bc9dbf43e214c17b4274b7ee
Sergio Miranda <sjamr10@gmail.com>
2019-03-18 19:12:12 -0300

    Add koa-router

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,13 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
-const app = new Koa(); 
+const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => { 
-  ctx.body = 'Hello World'; 
-}); 
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello World!';
+});
 
+app.use(router.routes());
+app.use(router.allowedMethods());
 app.listen(3000);
