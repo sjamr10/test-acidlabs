459f5276d3b18d72a91065212878e086dfa67f62
Sergio Miranda <sjamr10@gmail.com>
2019-03-18 19:14:29 -0300

    Add koa-logger

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,11 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const logger = require('koa-logger');
 
 const app = new Koa();
 const router = new Router();
+
+app.use(logger());
 
 router.get('/', (ctx, next) => {
   ctx.body = 'Hello World!';
